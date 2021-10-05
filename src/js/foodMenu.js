6b3b471000d0356f3545bc98f5bc1e92a7782f59
Vitaliy-Karpenko529 <vkarpenko945@gmail.com>
2021-10-05 23:35:00 +0300

    connected denam markup

---
+++ src/js/foodMenu.js
@@ -0,0 +1,20 @@
+import data from "../menu.json"
+import template from "../templates/myFood.hbs"
+import refs from "./refs"
+
+const { body, menu, checkbox } = refs
+const siteList = template(data);
+menu.insertAdjacentHTML("beforeend", siteList);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const { LIGHT, DARK } = Theme;
+const currentTheme = localStorage.getItem('theme') || LIGHT;
+
+localStorage.setItem('theme', currentTheme);
+body.classList.add(currentTheme);
+checkbox.checked = currentTheme === DARK;
+
